cfd1a21b488adef2b8af33b6562712c29171389a
y0y0 <y0y0.com>
2022-07-04 18:11:40 +0800

    third commit

--- src/main/java/git/GitTest01.java
+++ src/main/java/git/GitTest01.java
@@ -8,6 +8,7 @@ public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("猪币吧 这怎么这么菜啊");
         System.out.println("猪币吧 这怎么这么菜啊 version 2.0");
+        System.out.println("猪币吧 这怎么这么菜啊 version 3.0");
 
     }
 }
