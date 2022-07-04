c6b4902a34d1b74e7f27448f4f152b95c6551dba
y0y0 <y0y0.com>
2022-07-04 22:02:53 +0800

    hot-fix first commit

--- src/main/java/git/GitTest01.java
+++ src/main/java/git/GitTest01.java
@@ -9,6 +9,7 @@ public class GitTest01 {
         System.out.println("猪币吧 这怎么这么菜啊");
         System.out.println("猪币吧 这怎么这么菜啊 version 2.0");
         System.out.println("猪币吧 这怎么这么菜啊 version 3.0");
+        System.out.println("猪币吧 这怎么这么菜啊 version 4.0");
 
     }
 }
