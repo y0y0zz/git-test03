a990b2f8e489646b9ffb0594f3fa01122c3d1e8b
y0y0 <y0y0.com>
2022-07-04 22:06:40 +0800

    GitTest commit4.0

--- src/main/java/git/GitTest01.java
+++ src/main/java/git/GitTest01.java
@@ -11,6 +11,7 @@ public class GitTest01 {
         System.out.println("猪币吧 这怎么这么菜啊 version 3.0");
         System.out.println("猪币吧 这怎么这么菜啊 version 4.0");
         System.out.println("猪币吧 这怎么这么菜啊 version 5.0");
+        System.out.println("猪币吧 这怎么这么菜啊 version 6.0");
 
     }
 }
