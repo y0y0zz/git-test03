3f13251a189af10f440b0e2f52e4ef5a37d606ac
y0y0 <y0y0.com>
2022-07-05 20:01:37 +0800

    push commit

--- src/main/java/git/GitTest01.java
+++ src/main/java/git/GitTest01.java
@@ -7,14 +7,10 @@ package git;
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("猪币吧 这怎么这么菜啊");
-        System.out.println("猪币吧 这怎么这么菜啊 version 2.0");
-        System.out.println("猪币吧 这怎么这么菜啊 version 3.0");
-        System.out.println("猪币吧 这怎么这么菜啊 version 4.0");
-        System.out.println("猪币吧 这怎么这么菜啊 version 5.0");
-        System.out.println("猪币吧 这怎么这么菜啊 version 6.0");
-        System.out.println("猪币吧 这怎么这么菜啊 version 7.0");
         System.out.println("hot-fix test");
         System.out.println("insert master");
+        System.out.println("push test");
+
 
     }
 }
