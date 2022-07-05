ceeaea0aca6085afff1230a832dc7dd4fd8e2282
y0y0 <y0y0.com>
2022-07-05 15:15:47 +0800

    hot-fix delete

--- src/main/java/git/GitTest01.java
+++ src/main/java/git/GitTest01.java
@@ -12,7 +12,6 @@ public class GitTest01 {
         System.out.println("猪币吧 这怎么这么菜啊 version 4.0");
         System.out.println("猪币吧 这怎么这么菜啊 version 5.0");
         System.out.println("猪币吧 这怎么这么菜啊 version 6.0");
-        System.out.println("猪币吧 这怎么这么菜啊 version 7.0");
         System.out.println("hot-fix test");
 
     }
