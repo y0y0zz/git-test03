c5d5b180891468f74d1dabbd8947ee42debfe488
y0y0 <y0y0.com>
2022-07-05 20:08:18 +0800

    git-test02 push commit

--- src/main/java/git/GitTest01.java
+++ src/main/java/git/GitTest01.java
@@ -10,6 +10,6 @@ public class GitTest01 {
         System.out.println("hot-fix test");
         System.out.println("insert master");
         System.out.println("push test");
-
+        System.out.println("git-test02");
     }
 }
