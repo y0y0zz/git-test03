4354ab0e64f152c4c99e943cc90409d59cac56a8
y0y0zz <108585300+y0y0zz@users.noreply.github.com>
2022-07-05 20:37:27 +0800

    pull commit02

--- src/main/java/git/GitTest01.java
+++ src/main/java/git/GitTest01.java
@@ -11,5 +11,6 @@ public class GitTest01 {
         System.out.println("insert master");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test02");
     }
 }
